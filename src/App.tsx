577b7124e39a9580fb5f258dfbcd6cda8061d6f7
jmdatulsaxena <jmdatul@gmail.com>
2018-12-17 18:43:54 -0500

    change title

--- src/App.tsx
+++ src/App.tsx
@@ -84,7 +84,7 @@ class App extends React.Component<any, IAppState>{
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
+          <h1 className="App-title">Welcome to StarWars</h1>
         </header>
         <div>
           <div>
